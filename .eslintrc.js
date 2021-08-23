6cd6b30086f8333538f874bf0d2dd76abd489306
Galih Muhammad Ramdan <galihmramdan17@gmail.com>
2021-08-23 10:34:12 +0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': 'off',
+  },
+  plugins: ['prettier'],
+};
